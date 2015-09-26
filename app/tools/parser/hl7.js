1cb3681d8b0fffef32a2f26be0cc6d1c99759cd7
Simplicio Carcamo <siarcarse@gmail.com>
2015-09-26 15:12:02 -0300

    hl7 add function validateStream

--- app/tools/parser/hl7.js
+++ app/tools/parser/hl7.js
@@ -1,5 +1,6 @@
 
 import parser from 'L7';
+let config = require('../../../conf/config.js');
 
 export function parse(messageNative) {
     let runtime_errors = [];
@@ -13,6 +14,30 @@ export function parse(messageNative) {
             var messages = [parsedMessage, messageNative];
 
             resolve(messages);
+        }
+    });
+}
+export function validateStream(dataString) {
+    return new Promise((resolve, reject) => {
+        let posStart = dataString.indexOf(config.startFrame);
+        let posEnd = dataString.indexOf(config.endFrame);
+
+        while (posStart > -1 && posEnd > -1) {
+            if (posStart > -1 && posEnd > -1) {
+                // Aqui deberia estar todo el mensaje
+                if (posEnd < posStart) {
+                    //Si entra aca es porque se perdio parte del mensaje... Se borra
+                    dataString = dataString.substring(posEnd + config.endFrame.length);
+                    reject('ERROR: Mensaje Incompleto!');
+                } else {
+                    let msg = dataString.substring(posStart + posStart.length, posEnd).trim();
+                    resolve(msg);
+                    dataString = dataString.substring(posEnd + config.endFrame.length);
+                }
+            }
+
+            posStart = dataString.indexOf(config.startFrame);
+            posEnd = dataString.indexOf(config.endFrame);
         }
     });
 }
--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import net from 'net';
 let config = require('./conf/config.js');
-import * as fullStream from './app/tools/validate/stream.js';
 import * as hl7 from './app/tools/parser/hl7.js';
 import * as validate from './app/tools/validate/validate.js';
 import * as ack from './app/tools/ack/ack.js';
@@ -15,14 +14,13 @@ let Server = net.createServer((socket) => {
     socket.on('data', (data) => {
         dataString += data; // Autoincrementa el stream para obtener todo el mensaje HL7
         console.log("Received message");
-        fullStream.validateStream(dataString) //valido que llegue el mensaje completo
+        hl7.validateStream(dataString) //valido que llegue el mensaje completo
             .then(hl7.parse) // Parseo el mensaje
             .then(validate.test) // Valido en el ris
             .then(ack.buildAck) //Creo ACK con mensaje customizado
             .then((ack) => {
                 console.log("Sending ACK");
                 socket.write(ack); // Envio ACK
-                console.log(data);
             })
             .catch((err) => {
                 console.log(err); // Controlo los putos errores
